0cae9c89245a4366560030eaaeed3f13b3fe1ab1
Georgiy <parsegovgeorgiy@gmail.com>
2019-10-20 22:32:19 +0300

    Updates

---
+++ precache-manifest.e3ce882ea0269179c0184a549f1c006e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a251e458a97ad470248",
+    "url": "/taskManager/static/js/runtime~main.3a251e45.js"
+  },
+  {
+    "revision": "ca0e1e49cf54a38bc922",
+    "url": "/taskManager/static/js/main.ca0e1e49.chunk.js"
+  },
+  {
+    "revision": "04c11a764148e069fc86",
+    "url": "/taskManager/static/js/1.04c11a76.chunk.js"
+  },
+  {
+    "revision": "ca0e1e49cf54a38bc922",
+    "url": "/taskManager/static/css/main.89cf9c5e.chunk.css"
+  },
+  {
+    "revision": "04c11a764148e069fc86",
+    "url": "/taskManager/static/css/1.7aaedeef.chunk.css"
+  },
+  {
+    "revision": "aa0aaff72db952bc76cbabce3171edbc",
+    "url": "/taskManager/index.html"
+  }
+];
