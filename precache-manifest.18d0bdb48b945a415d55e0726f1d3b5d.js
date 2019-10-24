c0a21123eff385446a398ae95d56a3e626af2464
Барсуков Александр Дмитриевич <barsukov.a.d@sberbank.ru>
2019-10-24 14:03:33 +0300

    Updates

---
+++ precache-manifest.18d0bdb48b945a415d55e0726f1d3b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb31174a89f74d8e5b36da6d40efca2",
+    "url": "/taskManager/index.html"
+  },
+  {
+    "revision": "81b17601209d99eb7482",
+    "url": "/taskManager/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "f1f9ae4e44d4c0e92d77",
+    "url": "/taskManager/static/css/main.0e7d5041.chunk.css"
+  },
+  {
+    "revision": "81b17601209d99eb7482",
+    "url": "/taskManager/static/js/2.0117eec9.chunk.js"
+  },
+  {
+    "revision": "f1f9ae4e44d4c0e92d77",
+    "url": "/taskManager/static/js/main.d2652cc4.chunk.js"
+  },
+  {
+    "revision": "88484654b1943d7e36c7",
+    "url": "/taskManager/static/js/runtime-main.0ffdaf56.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taskManager/precache-manifest.e3ce882ea0269179c0184a549f1c006e.js"
+  "/taskManager/precache-manifest.18d0bdb48b945a415d55e0726f1d3b5d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/taskManager/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taskManager/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
