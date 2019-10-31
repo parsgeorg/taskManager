af37e75a4350ee65307eb2738bf780f79266b342
parsgeorg <parsegovgeorgiy@gmail.com>
2019-10-31 22:46:19 +0300

    Update index.js

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Router as ReactRouter, Switch } from "react-router-dom";
 import { history } from "../../history";
 import LoginForm from "../Auth/LoginForm";
-//import NotFound from "../NotFound";
+import NotFound from "../NotFound";
 import Tasks from "../Tasks";
 
 const Router = () => (
@@ -11,7 +11,7 @@ const Router = () => (
       <Route exact path="/" component={Tasks} />
       <Route path="/login" component={LoginForm} />
       <Route exact path="/tasks" component={Tasks} />
-//       <Route component={NotFound} />
+      <Route component={NotFound} />
     </Switch>
   </ReactRouter>
 );
